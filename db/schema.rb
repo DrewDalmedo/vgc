3ab0814184b91771e9cfa4f107bbf0833466f3bb
DrewDalmedo <andrewdalmedo@gmail.com>
2020-06-03 14:31:56 -0400

    Created games table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200603172539) do
+ActiveRecord::Schema.define(version: 20200603182851) do
+
+  create_table "games", force: :cascade do |t|
+    t.string  "title"
+    t.text    "description"
+    t.string  "genre"
+    t.string  "platform"
+    t.string  "developer"
+    t.string  "publisher"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
