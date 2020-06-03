e4e447bcb5b3abcbbfdb5a2da270b2812f113b46
DrewDalmedo <andrewdalmedo@gmail.com>
2020-06-03 15:29:32 -0400

    Added logout functionality and added more comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,37 @@
 # controls all interactions having to do with users, INCLUDING logging in and signing up
 class UsersController < ApplicationController
+    # registration
     get '/register' do
-        if session[:user_id].nil?
-            erb :'users/register'
+        if Helpers.is_logged_in?(session)
+            redirect to '/'
         else
+            erb :'users/register'
+        end
+    end
+
+    # TODO: make it so that you cannot create new users with identical info as previous users 
+    #       (username and email fields only)
+    post '/register' do
+        # if a text box isn't filled out...
+        if (params[:username].empty? || params[:email].empty? || params[:password].empty?)
+            redirect to '/register'
+        else # if all text boxes are filled in...
+            @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+            session[:user_id] = @user.id
             redirect to '/'
         end
     end
 
-    post '/register' do
-        if !(params[:username].empty? || params[:email].empty? || params[:password].empty?)
-            @user = User.create(params[:username], params[:email], params[:password])
-            session[:user_id] = @user.id
+    # logging in
+    get '/login' do
+        if Helpers.is_logged_in?(session)
             redirect to '/'
         end
-        
+    end
+
+    # logging out
+    get '/logout' do
+        session.clear
+        redirect to '/'
     end
 end
