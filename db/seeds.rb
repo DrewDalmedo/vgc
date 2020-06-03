1b5b35f5a7969c52e10fec6e0fbd5b7a7ea6f41b
DrewDalmedo <andrewdalmedo@gmail.com>
2020-06-03 14:26:35 -0400

    Created first migration and seed data

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+# data used for testing the app
+dovahkiin = User.create(username: "dragonborn", password: "skyrim4life", email: "dovah@kiin.com")
+titan_bt  = User.create(username: "bt-7274", password: "prepfortitanfall", email: "bt@imc.com")
+
