e1dcf39c4c075f49a89fd1672c527c91c7612e44
DrewDalmedo <andrewdalmedo@gmail.com>
2020-06-04 12:11:46 -0400

    Changed user_exists? to user_is_valid? to match functionality

--- app/models/helpers.rb
+++ app/models/helpers.rb
@@ -7,7 +7,7 @@ class Helpers
         end
     end
 
-    def self.user_exists?(username, password)
+    def self.user_is_valid?(username, password)
         user = User.all.find_by(username: username)
         binding.pry
         if !user.nil? && user.authenticate(password)
