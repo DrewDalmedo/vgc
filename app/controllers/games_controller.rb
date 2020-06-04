2bb860f5793775dfba20ee9a13b8dd3afae83d09
DrewDalmedo <andrewdalmedo@gmail.com>
2020-06-04 15:11:32 -0400

    Added @user variable for erb page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@ class GamesController < ApplicationController
     get '/games' do
         # shows the user's current games
         if Helpers.is_logged_in?(session)
+            @user = User.find(session[:user_id])
             erb :'games/users_games.erb'
         else
             redirect to '/login'
