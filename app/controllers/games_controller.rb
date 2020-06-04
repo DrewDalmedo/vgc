6a6e603f76c1f1b7d9a11cffa6563e043d282430
DrewDalmedo <andrewdalmedo@gmail.com>
2020-06-04 15:39:10 -0400

    Create link for new game creation

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,24 @@
 class GamesController < ApplicationController
+    # game collection view
     get '/games' do
         # shows the user's current games
         if Helpers.is_logged_in?(session)
             @user = User.find(session[:user_id])
-            erb :'games/users_games.erb'
+            erb :'games/users_games'
         else
             redirect to '/login'
         end
+    end
+
+    # new game creation
+    get '/games/new' do
+        
+        if Helpers.is_logged_in?(session)
+            @user = User.find(session[:user_id])
+            erb :'games/new'
+        else
+            redirect to '/login'
+        end
+        
     end
 end
